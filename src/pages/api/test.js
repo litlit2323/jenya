3b4c7e3ee311993f7b3278707ee8fa1a8a983da5
100pecheneK <mistermihail23@gmail.com>
2020-11-06 14:45:46 +0500

    a

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -10,11 +10,11 @@ export default apiRoutesHandler(
   withDb({
     GET: async (req, res) => {
       const test3 = new TestModel3({ title: 'test3 title' })
-      test3.save()
+      await test3.save()
       const test2 = new TestModel2({ title: 'test2 title', test3: test3.id })
-      test2.save()
+      await test2.save()
       const test1 = new TestModel({ title: 'test1 title', test2: test2.id })
-      test1.save()
+      await test1.save()
 
       const testData = await TestModel.find().populate('test2', ['test3'])
       return res.json({ testData })

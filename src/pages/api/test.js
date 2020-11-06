dbc20036ac7232a23aab3411115094f303647494
100pecheneK <mistermihail23@gmail.com>
2020-11-06 14:15:57 +0500

    a

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -2,11 +2,14 @@ import withDb from '@utils/dbConnect'
 import apiRoutesHandler from '@utils/apiRoutesHandler'
 import TestModel from './../../models/TestModel'
 import Order from './../../models/Order'
+import User from './../../models/User'
 
 export default apiRoutesHandler(
   withDb({
     GET: async (req, res) => {
-      const testData = await Order.find()
+      const testData = await User.findById(
+        '5fa50faf6bff5600088fa9e7'
+      ).populate('orders', ['status'])
       return res.json({ testData })
     },
   })

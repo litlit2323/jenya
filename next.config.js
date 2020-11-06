fb69b5881ee0d22a896d883cb376a6b585115435
100pecheneK <mistermihail23@gmail.com>
2020-11-06 13:24:42 +0500

    a

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
-const withImages = require("next-images")
+const withImages = require('next-images')
 const withFonts = require('next-fonts')
 require('dotenv').config()
 
-module.exports = withImages(withFonts({
+module.exports = withImages(
+  withFonts({
     env: {
-        // HOST_URL: process.env.HOST_URL,
-        HOST_URL: "https://avista.vercel.app",
-        PORT: process.env.PORT,
-        // MONGO_URI: "mongodb://localhost:27017/avista",
-        MONGO_URI: process.env.MONGO_URI,
-        EMAIL: process.env.EMAIL,
-        PASSWORD: process.env.PASSWORD,
-        EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
-        EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
-        SECRET: process.env.SECRET
+      // HOST_URL: process.env.HOST_URL,
+      HOST_URL: process.env.HOST_URL,
+      PORT: process.env.PORT,
+      // MONGO_URI: "mongodb://localhost:27017/avista",
+      MONGO_URI: process.env.MONGO_URI,
+      EMAIL: process.env.EMAIL,
+      PASSWORD: process.env.PASSWORD,
+      EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
+      EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
+      SECRET: process.env.SECRET,
     },
-}))
+  })
+)

75f4953135cc1e5d163c0a40f99a3c8958102f91
100pecheneK <mistermihail23@gmail.com>
2020-11-06 15:16:34 +0500

    a

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -8,12 +8,12 @@ import User from './../../models/User'
 export default apiRoutesHandler(
   withDb({
     GET: async (req, res) => {
-      // const test3 = new TestModel3({ title: 'test3 title' })
-      // await test3.save()
-      // const test2 = new TestModel2({ title: 'test2 title', test3: test3.id })
-      // await test2.save()
-      // const test22 = new TestModel2({ title: 'test22 title', test3: test3.id })
-      // await test22.save()
+      const test3 = new TestModel3({ title: 'test3 title' })
+      await test3.save()
+      const test2 = new TestModel2({ title: 'test2 title', test3: test3.id })
+      await test2.save()
+      const test22 = new TestModel2({ title: 'test22 title', test3: test3.id })
+      await test22.save()
       // const test1 = new TestModel({ title: 'test1 title' })
       // test1.test2.addToSet(test2.id)
       // test1.test2.addToSet(test22.id)

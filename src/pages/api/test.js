70d68bb1bade3665790588090850b0d334ed02c3
100pecheneK <mistermihail23@gmail.com>
2020-11-06 14:33:57 +0500

    a

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -9,7 +9,7 @@ export default apiRoutesHandler(
     GET: async (req, res) => {
       const testData = await User.findById(
         '5fa50faf6bff5600088fa9e7'
-      ).populate({ path: 'orders', populate: { path: 'status' } })
+      ).populate('orders', ['status'])
       return res.json({ testData })
     },
   })

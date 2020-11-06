a74873e583d99d053e42587ead65172b4b0633bd
100pecheneK <mistermihail23@gmail.com>
2020-11-06 14:09:44 +0500

    a

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,14 +1,12 @@
 import withDb from '@utils/dbConnect'
 import apiRoutesHandler from '@utils/apiRoutesHandler'
 import TestModel from './../../models/TestModel'
+import Order from './../../models/Order'
 
 export default apiRoutesHandler(
   withDb({
     GET: async (req, res) => {
-      const testData = new TestModel({
-        title: 'Test title aaa',
-      })
-      await testData.save()
+      const testData = await Order.find()
       return res.json({ testData })
     },
   })
